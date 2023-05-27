9208f611770dfa132b9aeb10b6665054246a5d76
Nurullo <khuwaidullaev@gmail.com>
2023-05-27 13:33:08 +0500

    chore: add link to main page

--- src/components/layout/header/header.layout.tsx
+++ src/components/layout/header/header.layout.tsx
@@ -1,14 +1,15 @@
 import classes from "./header.module.scss";
 import iconLogo from "../../../assets/icons/ic_logo.svg";
 import iconUserProfile from "../../../assets/icons/ic_person.svg";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <header className={classes["header"]}>
-      <div className={classes["header__logo"]}>
+      <NavLink to="/" className={classes["header__logo"]}>
         <img src={iconLogo} alt="Company's logo" />
         <span>Wrench CRM</span>
-      </div>
+      </NavLink>
       <div className={classes["header__userProfile"]}>
         <img src={iconUserProfile} alt="User profile's icon" />
         <span>Имя Фамилия</span>
